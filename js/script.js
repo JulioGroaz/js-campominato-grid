f81898b00c985005c38bf30ede30135120e73343
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-17 15:33:11 +0200

    Creata la function() con click che permetta la generazione della griglia e utilizzato innerHTML per ripulire il tutto se si clicca nuovamente

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('grid-generator').addEventListener(click, function(){
+
+    const gridContainer = document.getElementById('grid-container');
+    gridContainer.innerHTML = ''; //così da ripulire il container una volta che si clicca il grid generator.
+
+    
+})
